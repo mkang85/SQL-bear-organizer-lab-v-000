2f6d121eab6e9fc51ee3a259ae213ec294ddb953
Michael Kang <michaelkang85@gmail.com>
2018-04-02 17:07:38 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, color, temperament) VALUES ("Mr. Chocolate", 3, "Brown", "Angry");
+INSERT INTO bears (name, age, color, temperament) VALUES ("Rowdy", 5, "Black", "Angry");
+INSERT INTO bears (name, age, color, temperament) VALUES ("Tabitha", 2, "Red", "Mellow");
+INSERT INTO bears (name, age, color, temperament) VALUES ("Sergeant Brown", 1, "Blue", "Non-Chalant");
+INSERT INTO bears (name, age, color, temperament) VALUES ("Melissa", 7, "Grey", "Relaxed");
+INSERT INTO bears (name, age, color, temperament) VALUES ("Grinch", 3, "Brown", "Angry");
+INSERT INTO bears (name, age, color, temperament) VALUES ("Wendy", 1, "Purple", "Relaxed");
+INSERT INTO bears (name, age, color, temperament) VALUES (NULL, 1, "Purple", "Relaxed");
